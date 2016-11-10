7b83cd0008d9c848bbc2fb9069d0f26e93f72ae0
jorgeamaya <jeamaya1991@gmail.com>
2016-11-10 10:21:13 +0100

    Include an utility to check the quality of the assemblies

--- SamplingTest/quality.sh
+++ SamplingTest/quality.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-#Check the quality of the assemblies
-#Theshold can be changed inside awk
+#Description: Compare the quality of the assemblies
+#Written by: Jorge Eduardo Amaya Romero
+#Last time revised: 14-10-2016
 
-grep -v ",0" Results/Count_$1\.csv | 
-cut -d "," -f 2 | 
+#Theshold can be changed inside awk
+module load Biopython/1.65-foss-2016a-Python-2.7.11
+
+python Code/eval_assembly.py
+
+grep -v ",0$" Results/Scores.csv | 
+cut -d "," -f 3 | 
 sort -n -r | 
 head -n 1 |
 awk '{if ($1 >= 35) 
